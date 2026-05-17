3d54255c16630ea3cc13f4bb2f714e5d31c7248b
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-05-17 16:13:03 +0900

    Time: 8 ms (77.85%), Space: 25 MB (98.96%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -4,11 +4,11 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         ans = False
         n = len(arr)
-        dp = [-1] * n
+        visited = [-1] * n
 
         q = []
         q.append(start)
-        dp[start] = 0
+        visited[start] = 0
         while q:
             cur_index = q.pop()
             
@@ -19,11 +19,11 @@ class Solution:
             next_plus = cur_index + arr[cur_index]
             next_minus = cur_index - arr[cur_index]
 
-            if next_plus < n and dp[next_plus] == -1:
-                dp[next_plus] = dp[cur_index] + 1
+            if next_plus < n and visited[next_plus] == -1:
+                visited[next_plus] = visited[cur_index] + 1
                 q.append(next_plus)
-            if next_minus >= 0 and dp[next_minus] == -1:
-                dp[next_minus] = dp[cur_index] + 1
+            if next_minus >= 0 and visited[next_minus] == -1:
+                visited[next_minus] = visited[cur_index] + 1
                 q.append(next_minus)
             
         return ans

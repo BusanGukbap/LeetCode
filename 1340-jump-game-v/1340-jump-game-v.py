6bc2f0abff390541e0b922c11ae9ef137d1243f5
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-05-25 21:14:39 +0900

    Time: 277 ms (23.05%), Space: 20.9 MB (76.08%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxJumps(self, arr: List[int], d: int) -> int:
+        N = len(arr)
+        dp = [0]*N
+        
+        def dfs(pos):
+            if dp[pos] != 0:
+                return
+
+            dp[pos] = 1
+            
+            # 왼쪽으로 점프하기
+            next_pos = pos - 1
+            while next_pos >= 0 and pos - next_pos <= d and arr[pos] > arr[next_pos]:
+                dfs(next_pos)
+                dp[pos] = max(dp[pos], dp[next_pos] + 1)
+                next_pos -= 1
+            
+            # 오른쪽으로 점프하기
+            next_pos = pos + 1
+            while next_pos < N and next_pos - pos <= d and arr[pos] > arr[next_pos]:
+                dfs(next_pos)
+                dp[pos] = max(dp[pos], dp[next_pos] + 1)
+                next_pos += 1
+                
+        for i in range(N):
+            dfs(i)
+
+
+        return max(dp)

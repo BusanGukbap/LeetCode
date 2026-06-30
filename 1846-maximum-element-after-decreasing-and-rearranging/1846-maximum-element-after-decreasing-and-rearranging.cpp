d239fe2a49f24c1ca06024dfa10d56bb88a08edf
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-06-30 19:53:48 +0900

    Time: 12 ms (22.96%), Space: 55.1 MB (76.47%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        int n = arr.size();
+
+        arr[0] = 1;
+
+        for (int i=1; i<n; i++) {
+            if (arr[i] - arr[i-1] <= 1){
+                continue;
+            }
+
+            arr[i] = arr[i-1] + 1;
+        }
+
+
+        return arr[n-1];
+    }
+};

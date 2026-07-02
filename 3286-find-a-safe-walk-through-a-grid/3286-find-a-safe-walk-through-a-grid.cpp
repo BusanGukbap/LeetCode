5c7b6467e194fa29cee9c698b25c8c93b086f275
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-07-02 13:40:07 +0900

    Time: 175 ms (13.56%), Space: 30.3 MB (91.68%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> maze(m, vector<int>(n, -1));
+        
+        priority_queue<pair<int, pair<int, int>>> pq;
+        pq.push({grid[0][0], {0, 0}});
+
+        while (!pq.empty()) {
+            int value = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+
+            pq.pop();
+            
+            if (value >= health)
+                continue;
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n)
+                    continue;
+
+                int nvalue = value + grid[nx][ny];
+                if (nvalue >= health)
+                    continue;
+                
+                if (maze[nx][ny] != -1 && maze[nx][ny] <= nvalue)
+                    continue;
+                
+                maze[nx][ny] = nvalue;
+                
+                pq.push({nvalue, {nx, ny}});
+            }
+        }
+
+        // cout << maze[m-1][n-1];
+
+        if (health > maze[m-1][n-1] && maze[m-1][n-1] != -1)
+            return true;
+        else
+            return false;
+    }
+};
